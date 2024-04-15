eba3f17d2afac38a3c4eddf57059aa250ef027da
Eugenia Diaz <olivasdiazdanielaeugenia@gmail.com>
2024-04-15 08:21:59 -0600

    Proyecto finalizado

--- EstructurasRepetitivas/src/App.java
+++ EstructurasRepetitivas/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner e = new Scanner (System.in);
+        System.out.println("Ingrese su nombre");
+        String nombre;
+        nombre = e.nextLine();
+
+        System.out.println("Hola, "+nombre);
+        e.close();
+
     }
 }
